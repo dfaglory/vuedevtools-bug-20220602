6b68dde4fa5d29e7577125bd782cd42758542b16
dfaglory <30020100@qq.com>
2022-06-04 10:14:25 +0800

    bug

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+<template>
+  <keep-alive include="left">
+    <left></left>
+  </keep-alive>
+  <keep-alive>
+    <right></right>
+  </keep-alive>
+</template>
+<script setup>
+import left from "./views/left.vue";
+import right from "./views/right.vue";
+</script>
+<style></style>
